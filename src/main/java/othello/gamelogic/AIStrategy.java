0a80b901544f8245561c677f02b3975c23b4e393
owen <cj669@sas.upenn.edu>
2025-04-19 21:49:51 -0400

    Created the AIStrategy interface and declared the computerMove method

---
+++ src/main/java/othello/gamelogic/AIStrategy.java
@@ -0,0 +1,12 @@
+package othello.gamelogic;
+
+public interface AIStrategy {
+
+    /**
+     *
+     * @param board the Othello game board
+     * @param actingPlayer the computer player that is moving
+     * @return the determined board position for the computer player
+     */
+    BoardSpace computerMove(BoardSpace[][] board, Player actingPlayer);
+}
