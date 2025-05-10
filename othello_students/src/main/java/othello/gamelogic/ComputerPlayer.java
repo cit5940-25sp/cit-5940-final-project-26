d601ce260560e92857226d3d044f7cc2af20a7a4
March <marchk@myyahoo.com>
2025-05-10 11:34:49 -0500

    Coordinating the version of main

--- othello_students/src/main/java/othello/gamelogic/ComputerPlayer.java
+++ othello_students/src/main/java/othello/gamelogic/ComputerPlayer.java
@@ -1,20 +1,19 @@
 package othello.gamelogic;
 
-import java.util.List;
-import java.util.Map;
-
 /**
  * Represents a computer player that will make decisions autonomously during their turns.
  * Employs a specific computer strategy passed in through program arguments.
  */
 public class ComputerPlayer extends Player{
     private String strategy;
+    private AIStrategy theComputerStrategy;
 
     public ComputerPlayer(String strategyName) {
         // PART 2
         // TODO: Use the strategyName input to create a specific strategy class for this computer
         // This input should match the ones specified in App.java!
         this.strategy = strategyName;
+        theComputerStrategy = null;
     }
 
     // PART 2
@@ -28,5 +27,15 @@ public class ComputerPlayer extends Player{
             return next;
         }
         return null;
+    }
+
+    public BoardSpace computerMove(BoardSpace[][] board) {
+        // If the actual AI strategy is not instantiated, return null
+        if (theComputerStrategy == null) {
+            return null;
+        }
+
+        // Delegation
+        return theComputerStrategy.computerMove(board, this);
     }
 }
