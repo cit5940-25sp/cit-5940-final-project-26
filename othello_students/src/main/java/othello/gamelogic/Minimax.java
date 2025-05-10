42c671693e6819ef6704ee1aae5c31f3662c3df5
March <marchk@myyahoo.com>
2025-05-10 12:05:26 -0500

    Modified a little logic of Minimax Strategy

--- othello_students/src/main/java/othello/gamelogic/Minimax.java
+++ othello_students/src/main/java/othello/gamelogic/Minimax.java
@@ -24,30 +24,11 @@ public class Minimax {
                 board[i][j] = new BoardSpace(i, j, BoardSpace.SpaceType.EMPTY);
             }
         }
-        /*board[2][3].setType(BoardSpace.SpaceType.BLACK);
-        board[3][3].setType(BoardSpace.SpaceType.BLACK);
-        board[4][3].setType(BoardSpace.SpaceType.BLACK);
-        board[1][4].setType(BoardSpace.SpaceType.BLACK);
-        board[0][5].setType(BoardSpace.SpaceType.BLACK);
-        board[2][5].setType(BoardSpace.SpaceType.BLACK);
-        board[2][4].setType(BoardSpace.SpaceType.WHITE);
-        board[3][4].setType(BoardSpace.SpaceType.WHITE);
-        board[4][4].setType(BoardSpace.SpaceType.WHITE);
-
-         */
         board[3][3].setType(BoardSpace.SpaceType.WHITE);
         board[3][4].setType(BoardSpace.SpaceType.BLACK);
         board[4][3].setType(BoardSpace.SpaceType.BLACK);
         board[4][4].setType(BoardSpace.SpaceType.WHITE);
         //System.out.println(test.toString(board));
-
-        /*Map<BoardSpace, List<BoardSpace>> moves = pc.getAvailableMoves(board);
-        //System.out.println(moves.get())
-        Player one = new HumanPlayer();
-        Player two = new ComputerPlayer("");
-        test.buildTree(board, one, two);
-
-         */
         Node root = new Node();
         Player one = new HumanPlayer();
         Player two = new ComputerPlayer("");
@@ -66,55 +47,7 @@ public class Minimax {
 
     public Minimax() {}
 
-
-    /*
-
-    public BoardSpace miniMaxStrategy(Node node) {
-        if (!node.isRoot()) {
-            System.out.println("Invalid root node!");
-            return null;
-        }
-
-        for (Node root_child : node.getChildren()) {
-            if (root_child.getChildren().isEmpty()) {
-                continue;
-            }
-            Node min_node = findMin(root_child.getChildren());
-            root_child.setWeight(min_node.getWeight());
-        }
-        Node max_node = findMax(node.getChildren());
-        return max_node.getBoardspace();
-    }
-
-    public Node findMax(ArrayList<Node> children) {
-        Node max_node = children.get(0);
-        int max = max_node.getWeight();
-        for (Node n : children) {
-            if (n.getWeight() >= max) {
-                max_node = n;
-                max = n.getWeight();
-            }
-        }
-        return max_node;
-    }
-
-    public Node findMin(ArrayList<Node> children) {
-        Node min_node = children.get(0);
-        int min = min_node.getWeight();
-        for (Node n : children) {
-            if (n.getWeight() <= min) {
-                min_node = n;
-                min = n.getWeight();
-            }
-        }
-        return min_node;
-    }
-
-
-     */
-
-
-    //This method is the next step for buildTree
+    //This method is the next step after buildTree()
     //Return the next optimal BoardSpace to move based on minimax algo
     public BoardSpace minimaxStrategyWithMaxDepth(Node node) {
         Node next_node = value(node);
@@ -136,6 +69,7 @@ public class Minimax {
         return node;
     }
 
+    //In order to get the max node in the branch
     public Node max_node(Node node) {
         int max = Integer.MIN_VALUE;
         Node max_node = null;
@@ -152,6 +86,7 @@ public class Minimax {
         return max_node;
     }
 
+    //In order to get the min node in the branch
     public Node min_node(Node node) {
         int min = Integer.MAX_VALUE;
         Node min_node = null;
@@ -170,12 +105,13 @@ public class Minimax {
 
 
     //Depth start from 0
+    //MaxDepth starts from 2 to satisfy the minimax
     //This method is to build the future state tree from current board state
     public void buildTree(BoardSpace[][] board, Player pc, Player op, int depth, Node root, int maxDepth) {
-        BoardSpace[][] copyBoard = this.getCopyBoard(board);
-        if (depth > maxDepth) {
-            return;
+        if (maxDepth < 2) {
+            throw new IllegalArgumentException("Error: Max Depth should be at least 2");
         }
+        BoardSpace[][] copyBoard = this.getCopyBoard(board);
         if (depth == 0) {
             root.setRoot(true);
             root.setDepth(depth);
@@ -184,9 +120,9 @@ public class Minimax {
             return;
         }
         if (depth % 2 != 0) {
-            //This is pc
+            //This is pc (self)
             Map<BoardSpace, List<BoardSpace>> availableMoves = pc.getAvailableMoves(copyBoard);
-            if (availableMoves.isEmpty()) {
+            if (availableMoves.isEmpty() || depth + 1 > maxDepth) {
                 root.setWeight(computeWeight(copyBoard,pc));
                 return;
             }
@@ -202,7 +138,7 @@ public class Minimax {
         } else {
             //This is op
             Map<BoardSpace, List<BoardSpace>> availableMoves = op.getAvailableMoves(copyBoard);
-            if (availableMoves.isEmpty()) {
+            if (availableMoves.isEmpty() || depth + 1 > maxDepth) {
                 root.setWeight(computeWeight(copyBoard,pc));
                 return;
             }
@@ -218,50 +154,6 @@ public class Minimax {
         }
     }
 
-
-    /*
-    //Build a game tree with max depth to be 2, and it can predict the next best option
-    public Node buildTree2(BoardSpace[][] board, Player pc, Player op) {
-        //We need to get the copy of original board to predict
-        BoardSpace[][] copyBoard = this.getCopyBoard(board);
-        //The minimax strategy will be preformed below to get next BoardSpace to go
-        Map<BoardSpace, List<BoardSpace>> availableMoves = pc.getAvailableMoves(copyBoard);
-        //If there is no available move, return null
-        Node root = new Node();
-        root.setRoot(true);
-        root.setDepth(0);
-        ArrayList<Node> root_children = new ArrayList<>();
-        for (BoardSpace space : availableMoves.keySet()) {
-            Node tempNode = new Node();
-            tempNode.setBoardSpace(space);
-            tempNode.setDepth(1);
-            tempNode.setParent(root);
-            tempNode.setMax(true);
-            BoardSpace[][] future = futureBoard(copyBoard,space,availableMoves,pc);
-            tempNode.setWeight(computeWeight(future,pc));
-            Map<BoardSpace, List<BoardSpace>> availableMoves_op = op.getAvailableMoves(future);
-            ArrayList<Node> children = new ArrayList<>();
-            for (BoardSpace space_op : availableMoves_op.keySet()) {
-                Node op_node = new Node();
-                op_node.setBoardSpace(space_op);
-                op_node.setDepth(2);
-                op_node.setParent(tempNode);
-                op_node.setMin(true);
-                BoardSpace[][] future_op = futureBoard(future, space_op, availableMoves_op, op);
-                int weight_op = computeWeight(future_op, pc);
-                op_node.setWeight(weight_op);
-                children.add(op_node);
-            }
-            tempNode.setChildren(children);
-            root_children.add(tempNode);
-        }
-        root.setChildren(root_children);
-        return root;
-    }
-
-     */
-
-
     //This method is for creating a copy of current state of board
     //In order to prevent any modification for original board
     public BoardSpace[][] getCopyBoard(BoardSpace[][] original) {
@@ -274,6 +166,7 @@ public class Minimax {
         return copyBoard;
     }
 
+    //This method is to compute the weight of each node of current board state
     public int computeWeight(BoardSpace[][] board, Player pc) {
         int self_score = 0;
         int opponent_score = 0;
@@ -291,6 +184,7 @@ public class Minimax {
         return (self_score - opponent_score);
     }
 
+    //This method is to show the next stage of the board by taking space on destination
     public BoardSpace[][] futureBoard(BoardSpace[][] board, BoardSpace destination, Map<BoardSpace,
             List<BoardSpace>> availableMoves, Player pc) {
         BoardSpace[][] copyBoard = getCopyBoard(board);
@@ -325,6 +219,7 @@ public class Minimax {
         return copyBoard;
     }
 
+    //This method is for showing the board state in a clear way
     public String toString(BoardSpace[][] board) {
         String board_str = "";
         for (int i = 0; i < board.length; i++) {
