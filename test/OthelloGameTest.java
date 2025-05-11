eee0888bc6897d985db37ecfeb666569f6c48e8b
realredk <bzredk@upenn.edu>
2025-05-11 10:21:18 -0400

    addtestbackNfront

--- test/OthelloGameTest.java
+++ test/OthelloGameTest.java
@@ -68,15 +68,40 @@ public class OthelloGameTest {
     }
 
     @Test
-    public void frontFlipAt23() {
-        // Arrange: build custom column and compute moves
-        // clear center four
-        for (int x=3; x<=4; x++)
-            for (int y=3; y<=4; y++)
-                board[x][y].setType(EMPTY);
+    public void frontNBackFlip() {
 
+        // run two cases: front (dest 2) and back (dest 6)
+        for (int destX : new int[]{2, 6}) {
 
+            /* reset column y=3 */
+            for (int x = 0; x < 8; x++)
+                board[x][3].setType(BoardSpace.SpaceType.EMPTY);
+
+            /* build W-W-W + B anchor */
+            int anchorX = (destX == 2) ? 6 : 2;
+            board[3][3].setType(BoardSpace.SpaceType.WHITE);
+            board[4][3].setType(BoardSpace.SpaceType.WHITE);
+            board[5][3].setType(BoardSpace.SpaceType.WHITE);
+            board[anchorX][3].setType(BoardSpace.SpaceType.BLACK);
+
+            /* only (destX,3) should be legal */
+            Map<BoardSpace,List<BoardSpace>> moves = game.getAvailableMoves(black);
+            assertEquals(1, moves.size());
+            assertTrue(moves.containsKey(board[destX][3]));
+
+            /* flip */
+            game.takeSpaces(black, white, moves, board[destX][3]);
+
+            /* column 2-6 turns black, rest stays empty */
+            for (int x = 0; x < 8; x++) {
+                for (int y = 0; y < 8; y++) {
+                    if (y == 3 && x >= 2 && x <= 6) {
+                        assertSame(BoardSpace.SpaceType.BLACK, board[x][y].getType());
+                    } else {
+                        assertSame(BoardSpace.SpaceType.EMPTY, board[x][y].getType());
+                    }
+                }
+            }
+        }
     }
-
-
 }
