073deec7e32b8c2e23f9a320f688fed9fffc84c2
owen <cj669@sas.upenn.edu>
2025-05-11 23:54:34 -0400

    Modified getAvailableMoves method

--- src/main/java/othello/gamelogic/Player.java
+++ src/main/java/othello/gamelogic/Player.java
@@ -50,43 +50,43 @@ public abstract class Player {
                 {-1, -1}, {-1, 0}, {-1, 1}, {0, -1}, {0, 1}, {1, -1}, {1, 0}, {1, 1}
         };
 
-        for (int row = 0; row < board.length; row++) {
-            for (int column = 0; column < board[row].length; column++) {
+        for (int x = 0; x < board.length; x++) {
+            for (int y = 0; y < board[x].length; y++) {
                 // If specific position is empty ...
-                if (board[row][column].getType() == BoardSpace.SpaceType.EMPTY) {
+                if (board[x][y].getType() == BoardSpace.SpaceType.EMPTY) {
                     List<BoardSpace> theOrigins = new ArrayList<>();
                     for (int[] eachDirection : availableDirections) {
-                        int rowChange = eachDirection[0];
-                        int columnChange = eachDirection[1];
+                        int xChange = eachDirection[0];
+                        int yChange = eachDirection[1];
 
-                        int newRow = row + rowChange;
-                        int newColumn = column + columnChange;
+                        int newX = x + xChange;
+                        int newY = y + yChange;
 
                         List<BoardSpace> opponentPositions = new ArrayList<>();
                         // Check the new position -- must be both within valid bounds
                         // and belong to the opponent
-                        while (checkBounds(newRow, newColumn, board.length) &&
-                        board[newRow][newColumn].getType() == opponent) {
+                        while (checkBounds(newX, newY, board.length) &&
+                        board[newX][newY].getType() == opponent) {
                             // Add the opponent's positions to the list
-                            opponentPositions.add(board[newRow][newColumn]);
+                            opponentPositions.add(board[newX][newY]);
                             // Then we continue moving further in this direction to check more positions
-                            newRow += rowChange;
-                            newColumn += columnChange;
+                            newX += xChange;
+                            newY += yChange;
                         }
 
                         // When we complete checking the series of opponent positions,
                         // we check if the ending position belongs to this player
-                        if (!opponentPositions.isEmpty() && checkBounds(newRow, newColumn, board.length) &&
-                        board[newRow][newColumn].getType() == this.getColor()) {
+                        if (!opponentPositions.isEmpty() && checkBounds(newX, newY, board.length) &&
+                        board[newX][newY].getType() == this.getColor()) {
                             // Add the origin position to the list
-                            theOrigins.add(board[newRow][newColumn]);
+                            theOrigins.add(board[newX][newY]);
                         }
                     }
 
                     // If in the end, there exists valid direction for our destination,
                     // then put the (destination, list of origin positions) into the map
                     if (!theOrigins.isEmpty()) {
-                        allAvailableMoves.put(board[row][column], theOrigins);
+                        allAvailableMoves.put(board[x][y], theOrigins);
                     }
                 }
             }
